cf0f4bb77c508f8fec97e2abe59b96820d2f0ecd
Relsell <relsellglobal@gmail.com>
2018-06-06 12:23:15 +0530

    initial project

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} : ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err) => {
+    if(err) {
+      console.log('Unable to append file');
+    }
+  });
+  next();
+})
+
+app.use((req,res,next) => {
+  res.render('maintenance.hbs',{
+    pageTitle: 'Maintenance Page'
+  });
+});
+
+
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+})
+
+app.get('/',(req,res) => {
+  // res.send('<h1>Hello Express</h1>');
+  // res.send({
+  //   name:'Anil',
+  //   likes:[
+  //     'Cities',
+  //     'Biking'
+  //   ]
+  // })
+
+  res.render('home.hbs',{
+    pageTitle:'Home',
+    welcomeMessage: 'Hello Welcome to page'
+
+  })
+
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About us'
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    code:'404',
+    messag: 'Hey buddy . I cannot find it'
+  })
+});
+
+app.listen(5000,() => {
+  console.log('Server is up on port 5000');
+});
